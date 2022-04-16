cbe7c6045ecda81b0090bb7d7fec5fa13bbc08c8
Mattew <20FTT1441@student.pb.edu.bn>
2022-04-16 09:58:43 +0800

    vuejs and tailwindcss installed

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    
+    <div id="info" class="shadow-md bg-gray-300 w-80 h-100 rounded-lg">
+    <img src="./assets/my_image2.jpeg" class="h-48 w-96 m-auto shadow-md bg-gray-300 w-20 h-24 rounded-full" alt="No image available" >
+      <p class="text-2xl font-bold">Mattew Lingie Anak Minggat</p>
+      <p>20FTT1441</p>
+      <p>D.O.B: 06/01/2000</p>
+      <p>School: School of Information and Technology</p>
+      <p>Institution: Politeknik Brunei</p>
+      <p>Course: Diploma in Web Development</p>
+    </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
